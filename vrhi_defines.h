d83202f7cb1e102034bcfd5fdc3a3d3439c73c0f
Xi Ma Chen <hypernewbie@gmail.com>
2026-01-02 02:18:08 -0800

    Add comprehensive sampler tests and update sampler definitions

--- vrhi_defines.h
+++ vrhi_defines.h
@@ -62,6 +62,7 @@
 #define VRHI_TEXTURE_BLIT_DST                     UINT64_C(0x0000400000000000) //!< Texture will be used as blit destination.
 //#define VRHI_TEXTURE_READ_BACK                    UINT64_C(0x0000800000000000) //!< Texture will be used for read back from GPU.
 
+#define VRHI_SAMPLER_U_WRAP                       UINT32_C(0x00000000) //!< Wrap U mode: Wrap
 #define VRHI_SAMPLER_U_MIRROR                     UINT32_C(0x00000001) //!< Wrap U mode: Mirror
 #define VRHI_SAMPLER_U_CLAMP                      UINT32_C(0x00000002) //!< Wrap U mode: Clamp
 #define VRHI_SAMPLER_U_BORDER                     UINT32_C(0x00000003) //!< Wrap U mode: Border
@@ -69,6 +70,7 @@
 
 #define VRHI_SAMPLER_U_MASK                       UINT32_C(0x00000003)
 
+#define VRHI_SAMPLER_V_WRAP                       UINT32_C(0x00000000) //!< Wrap V mode: Wrap
 #define VRHI_SAMPLER_V_MIRROR                     UINT32_C(0x00000004) //!< Wrap V mode: Mirror
 #define VRHI_SAMPLER_V_CLAMP                      UINT32_C(0x00000008) //!< Wrap V mode: Clamp
 #define VRHI_SAMPLER_V_BORDER                     UINT32_C(0x0000000c) //!< Wrap V mode: Border
@@ -76,6 +78,7 @@
 
 #define VRHI_SAMPLER_V_MASK                       UINT32_C(0x0000000c)
 
+#define VRHI_SAMPLER_W_WRAP                       UINT32_C(0x00000000) //!< Wrap W mode: Wrap
 #define VRHI_SAMPLER_W_MIRROR                     UINT32_C(0x00000010) //!< Wrap W mode: Mirror
 #define VRHI_SAMPLER_W_CLAMP                      UINT32_C(0x00000020) //!< Wrap W mode: Clamp
 #define VRHI_SAMPLER_W_BORDER                     UINT32_C(0x00000030) //!< Wrap W mode: Border
@@ -83,46 +86,61 @@
 
 #define VRHI_SAMPLER_W_MASK                       UINT32_C(0x00000030)
 
+#define VRHI_SAMPLER_MIN_LINEAR                   UINT32_C(0x00000000) //!< Min sampling mode: Linear
 #define VRHI_SAMPLER_MIN_POINT                    UINT32_C(0x00000040) //!< Min sampling mode: Point
 #define VRHI_SAMPLER_MIN_ANISOTROPIC              UINT32_C(0x00000080) //!< Min sampling mode: Anisotropic
 #define VRHI_SAMPLER_MIN_SHIFT                    6
 
 #define VRHI_SAMPLER_MIN_MASK                     UINT32_C(0x000000c0)
 
+#define VRHI_SAMPLER_MAG_LINEAR                   UINT32_C(0x00000000) //!< Mag sampling mode: Linear
 #define VRHI_SAMPLER_MAG_POINT                    UINT32_C(0x00000100) //!< Mag sampling mode: Point
 #define VRHI_SAMPLER_MAG_ANISOTROPIC              UINT32_C(0x00000200) //!< Mag sampling mode: Anisotropic
 #define VRHI_SAMPLER_MAG_SHIFT                    8
 
 #define VRHI_SAMPLER_MAG_MASK                     UINT32_C(0x00000300)
 
+#define VRHI_SAMPLER_MIP_LINEAR                   UINT32_C(0x00000000) //!< Mip sampling mode: Linear
 #define VRHI_SAMPLER_MIP_POINT                    UINT32_C(0x00000400) //!< Mip sampling mode: Point
+#define VRHI_SAMPLER_MIP_NONE                     UINT32_C(0x00000800) //!< Mip sampling mode: None
 #define VRHI_SAMPLER_MIP_SHIFT                    10
 
-#define VRHI_SAMPLER_MIP_MASK                     UINT32_C(0x00000400)
+#define VRHI_SAMPLER_MIP_MASK                     UINT32_C(0x00000c00)
 
-#define VRHI_SAMPLER_COMPARE_LESS                 UINT32_C(0x00010000) //!< Compare when sampling depth texture: less.
-#define VRHI_SAMPLER_COMPARE_LEQUAL               UINT32_C(0x00020000) //!< Compare when sampling depth texture: less or equal.
-#define VRHI_SAMPLER_COMPARE_EQUAL                UINT32_C(0x00030000) //!< Compare when sampling depth texture: equal.
-#define VRHI_SAMPLER_COMPARE_GEQUAL               UINT32_C(0x00040000) //!< Compare when sampling depth texture: greater or equal.
-#define VRHI_SAMPLER_COMPARE_GREATER              UINT32_C(0x00050000) //!< Compare when sampling depth texture: greater.
-#define VRHI_SAMPLER_COMPARE_NOTEQUAL             UINT32_C(0x00060000) //!< Compare when sampling depth texture: not equal.
-#define VRHI_SAMPLER_COMPARE_NEVER                UINT32_C(0x00070000) //!< Compare when sampling depth texture: never.
-#define VRHI_SAMPLER_COMPARE_ALWAYS               UINT32_C(0x00080000) //!< Compare when sampling depth texture: always.
-#define VRHI_SAMPLER_COMPARE_SHIFT                16
+#define VRHI_SAMPLER_COMPARE_LESS                 UINT32_C(0x00001000) //!< Compare when sampling depth texture: less.
+#define VRHI_SAMPLER_COMPARE_LEQUAL               UINT32_C(0x00002000) //!< Compare when sampling depth texture: less or equal.
+#define VRHI_SAMPLER_COMPARE_EQUAL                UINT32_C(0x00003000) //!< Compare when sampling depth texture: equal.
+#define VRHI_SAMPLER_COMPARE_GEQUAL               UINT32_C(0x00004000) //!< Compare when sampling depth texture: greater or equal.
+#define VRHI_SAMPLER_COMPARE_GREATER              UINT32_C(0x00005000) //!< Compare when sampling depth texture: greater.
+#define VRHI_SAMPLER_COMPARE_NOTEQUAL             UINT32_C(0x00006000) //!< Compare when sampling depth texture: not equal.
+#define VRHI_SAMPLER_COMPARE_NEVER                UINT32_C(0x00007000) //!< Compare when sampling depth texture: never.
+#define VRHI_SAMPLER_COMPARE_ALWAYS               UINT32_C(0x00008000) //!< Compare when sampling depth texture: always.
+#define VRHI_SAMPLER_COMPARE_SHIFT                12
 
-#define VRHI_SAMPLER_COMPARE_MASK                 UINT32_C(0x000f0000)
+#define VRHI_SAMPLER_COMPARE_MASK                 UINT32_C(0x0000f000)
+
+#define VRHI_SAMPLER_MIPBIAS_SHIFT                16
+#define VRHI_SAMPLER_MIPBIAS_MASK                 UINT32_C(0x00ff0000)
+#define VRHI_SAMPLER_MIPBIAS(v) ( ( (uint32_t)((v)*16.0f) << VRHI_SAMPLER_MIPBIAS_SHIFT )&VRHI_SAMPLER_MIPBIAS_MASK)
 
 #define VRHI_SAMPLER_BORDER_COLOR_SHIFT           24
 
 #define VRHI_SAMPLER_BORDER_COLOR_MASK            UINT32_C(0x0f000000)
 #define VRHI_SAMPLER_BORDER_COLOR(v) ( ( (uint32_t)(v)<<VRHI_SAMPLER_BORDER_COLOR_SHIFT )&VRHI_SAMPLER_BORDER_COLOR_MASK)
 
-#define VRHI_SAMPLER_RESERVED_SHIFT               28
+#define VRHI_SAMPLER_SAMPLE_STENCIL               UINT32_C(0x10000000) //!< Sample stencil instead of depth.
 
-#define VRHI_SAMPLER_RESERVED_MASK                UINT32_C(0xf0000000)
+#define VRHI_SAMPLER_MAX_ANISOTROPY_SHIFT         29
+#define VRHI_SAMPLER_MAX_ANISOTROPY_MASK          UINT32_C(0xe0000000)
+#define VRHI_SAMPLER_MAX_ANISOTROPY(v) ( ( (uint32_t)(v)<<VRHI_SAMPLER_MAX_ANISOTROPY_SHIFT )&VRHI_SAMPLER_MAX_ANISOTROPY_MASK)
+#define VRHI_SAMPLER_ANISOTROPY_1                 VRHI_SAMPLER_MAX_ANISOTROPY(0)
+#define VRHI_SAMPLER_ANISOTROPY_2                 VRHI_SAMPLER_MAX_ANISOTROPY(1)
+#define VRHI_SAMPLER_ANISOTROPY_4                 VRHI_SAMPLER_MAX_ANISOTROPY(2)
+#define VRHI_SAMPLER_ANISOTROPY_8                 VRHI_SAMPLER_MAX_ANISOTROPY(3)
+#define VRHI_SAMPLER_ANISOTROPY_16                VRHI_SAMPLER_MAX_ANISOTROPY(4)
 
 #define VRHI_SAMPLER_NONE                         UINT32_C(0x00000000)
-#define VRHI_SAMPLER_SAMPLE_STENCIL               UINT32_C(0x00100000) //!< Sample stencil instead of depth.
+
 #define VRHI_SAMPLER_POINT (0 \
 	| VRHI_SAMPLER_MIN_POINT \
 	| VRHI_SAMPLER_MAG_POINT \
@@ -147,6 +165,12 @@
 	| VRHI_SAMPLER_W_BORDER \
 	)
 
+#define VRHI_SAMPLER_UVW_WRAP (0 \
+	| VRHI_SAMPLER_U_WRAP \
+	| VRHI_SAMPLER_V_WRAP \
+	| VRHI_SAMPLER_W_WRAP \
+	)
+
 #define VRHI_SAMPLER_BITS_MASK (0 \
 	| VRHI_SAMPLER_U_MASK \
 	| VRHI_SAMPLER_V_MASK \
@@ -155,6 +179,10 @@
 	| VRHI_SAMPLER_MAG_MASK \
 	| VRHI_SAMPLER_MIP_MASK \
 	| VRHI_SAMPLER_COMPARE_MASK \
+	| VRHI_SAMPLER_MIPBIAS_MASK \
+	| VRHI_SAMPLER_BORDER_COLOR_MASK \
+	| VRHI_SAMPLER_SAMPLE_STENCIL \
+	| VRHI_SAMPLER_MAX_ANISOTROPY_MASK \
 	)
 
 // --- STATE ---
