0e0ab1e74adb84388a14fb18ecb639f8e9dfc5ad
Xi Ma Chen <hypernewbie@gmail.com>
2026-07-31 23:44:16 -0700

    bump: vrhi v0.8.2 -> v0.8.3

--- vrhi.h
+++ vrhi.h
@@ -175,7 +175,7 @@ extern std::atomic<int32_t> g_vhPSOCompileCounter;
 
 #define VRHI_VERSION_MAJOR 0
 #define VRHI_VERSION_MINOR 8
-#define VRHI_VERSION_PATCH 2
+#define VRHI_VERSION_PATCH 3
 
 #define VRHI_INVALID_HANDLE 0xFFFFFFFF
 #define VRHI_MIPMAP_COMPLETE -1
